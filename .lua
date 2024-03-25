47c3d02424db1bf71fcaac6499d310b2a6a95785
Reach Hub <162890945+ReachHublua@users.noreply.github.com>
2024-03-25 22:43:39 +0700

    Update .lua

--- .lua
+++ .lua
@@ -45,6 +45,60 @@ blur:Destroy()
 A:Destroy()
 
 
+local SafeZone = Instance.new("Part")
+SafeZone.Size = Vector3.new(10,3,10)
+SafeZone.Position = Vector3.new(-70, 50, 8756)
+SafeZone.Transparency = 1
+SafeZone.Material = "SmoothPlastic"
+SafeZone.Anchored = true
+SafeZone.Parent = game:GetService("Workspace")
+SafeZone.Name = "SafeZone"
+
+-- Global Varible
+
+_G.WaitingF = false
+_G.AutoFarm = false
+_G.Fastmode = true
+
+-- Functions
+
+
+function Automaticfarm(Fast)
+if Fast then
+_G.WaitingF = true
+  game.workspace.Gravity = 0
+
+    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame =
+        CFrame.new(-50, 52, -269)
+    tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(30, Enum.EasingStyle.Linear)
+        tween =
+        tweenService:Create(
+        game:GetService("Players")["LocalPlayer"].Character:WaitForChild("HumanoidRootPart"),
+        tweenInfo,
+        {CFrame = CFrame.new(-70, 53, 8756)}
+    )
+    tween:Play()
+    tween.Completed:Connect(function()
+    repeat wait()
+    pcall(function()
+        game.workspace.Gravity = 192
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = SafeZone.CFrame * CFrame.new(0,1.5,0)
+    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+    wait()
+    firetouchinterest(workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+    end)
+    until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid") == nil
+    repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+    _G.WaitingF = false
+    end)
+end
+end
+
+
+
+
+
+
 
 
 repo = 'https://raw.githubusercontent.com/ReachHublua/UI/main/'
@@ -138,6 +192,15 @@ end
 })
 
 
+spawn(function()
+while wait(0.5) do
+if _G.AutoFarm then
+if _G.WaitingF == false then
+        Automaticfarm(_G.Fastmode)
+    end
+end
+end
+end)
 
 
 
